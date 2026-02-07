9e4a32f51b6d666c451fcb12aece02963feae92e
vernoxy <vernoxy3@gmail.com>
2026-02-07 17:30:17 +0530

    Home page work

--- src/screens/home/Choose.jsx
+++ src/screens/home/Choose.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import whyChooseIcon1 from "../../assets/whyChooseIcon1.png";
 import whyChooseIcon2 from "../../assets/whyChooseIcon2.png";
 import whyChooseIcon3 from "../../assets/whyChooseIcon3.png";
@@ -19,59 +19,52 @@ const cardData = [
     title: "Personalized Consultations",
     desc: "Expert guidance tailored to your unique vision and eyewear needs.",
   },
+  {
+    icon: whyChooseIcon2,
+    title: "Affordable Pricing",
+    desc: "Transparent, competitive prices without compromisingquality or service",
+  },
+  {
+    icon: whyChooseIcon3,
+    title: "Curated Eyewear Brands",
+    desc: "Thousands of happy customers and glowing reviews-your satisfaction, our priority",
+  },
 ];
 
 const Choose = () => {
   return (
-    <div className="py-10 md:py-20 space-y-4 md:space-y-8 container xl:px-10">
-      <h1 className="text-[32px] text-3xl md:text-5xl xl:text-6xl text-[#06213c] font-kaisei_Decol font-normal text-center sm:mb-10">
-        Why Choose Us?
-      </h1>
-      <p data-aos="zoom"  className="text-sm md:text-xl lg:text-2xl text-[#334155] font-normal font-kaisei_Decol text-center max-w-4xl mx-auto">
-        Experience the difference with our commitment to vision, style, and genuine care —
-        trusted by thousands of happy customers.
-      </p>
-
-      <div
-        className=" bg-[#becfd6] bg-cover bg-center gap-[30px] flex flex-col items-center justify-between my-4 sm:my-[50px] rounded-[10px] shadow-[inset_0_30px_30px_-20px_rgba(0,0,0,0.5)] py-[20px]"
-      >
-        {/* First row (3 cards) */}
-        <div data-aos="zoom-in"  className="flex flex-col lg:flex-row items-center justify-between w-[90%] gap-6">
+    <>
+      
+      <section className="container">
+        <div className="grid gap-4 md:gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
+          <div className="space-y-5">
+            <h1 className="text-4xl xl:text-5xl text-[#06213c] font-kaisei_Decol font-normal">
+              Why Choose Us ?
+            </h1>
+            <p className="text-xl xl:text-2xl font-kanit 2xl:pe-20">Experience the difference with our commitment to vision, style, and genuine care — trusted by thousands of happy customers.</p>
+            <hr className="border-primary border-2 rounded-full w-32"/>
+          </div>
           {cardData.map((card, index) => (
             <div
               key={index}
-              className="flex flex-col items-center justify-center bg-white h-[250px] w-full lg:w-[30%] text-center rounded-[10px] gap-[20px] p-4"
+              className="bg-[#F8F8F8] hover:bg-primary duration-300 transition-colors rounded-2xl p-8 flex flex-col items-center justify-center gap-6 text-center hover:shadow-2xl group"
             >
-              <img src={card.icon} alt={`icon${index}`} className="h-[50px] w-[60px]" />
-              <h1 className="text-xl sm:text-[30px] text-[#06213c] font-medium font-kaisei_Decol">
+              <img
+                src={card.icon}
+                alt={`icon${index}`}
+                className="h-[50px] w-[60px]"
+              />
+              <h1 className="text-xl sm:text-[30px] text-[#06213c] group-hover:text-white duration-300 transition-colors font-kaisei_Decol">
                 {card.title}
               </h1>
-              <p className=" sm:text-[20px] text-[#06213c] font-normal font-kanit">{card.desc}</p>
-            </div>
-          ))}
-        </div>
-
-        {/* Second row (2 cards) */}
-        <div data-aos="zoom-in" className="flex flex-col md:flex-row items-center justify-between w-[90%] gap-6">
-          {[whyChooseIcon1, whyChooseIcon2].map((icon, idx) => (
-            <div
-              key={idx}
-              className="flex flex-col items-center justify-center bg-white h-[250px] w-full md:w-[48%] text-center rounded-[10px] gap-[20px] p-4"
-            >
-              <img src={icon} alt={`whyChooseIcon${idx + 1}`} className="h-[50px] w-[60px]" />
-              <h1 className="text-xl sm:text-[30px] text-[#06213c] font-medium font-kaisei_Decol">
-                {idx === 0 ? "State-of-the-art Testing" : "Curated Eyewear Brands"}
-              </h1>
-              <p className="sm:text-[20px] text-[#06213c] font-normal font-kanit">
-                {idx === 0
-                  ? "Advanced technology ensures precise, reliable eye exams for optimal vision care."
-                  : "Stylish, high-quality eyewear from thoughtfully selected premium brands."}
+              <p className=" sm:text-[20px] text-[#06213c] group-hover:text-white duration-300  transition-colors font-normal font-kanit">
+                {card.desc}
               </p>
             </div>
           ))}
         </div>
-      </div>
-    </div>
+      </section>
+    </>
   );
 };
 
